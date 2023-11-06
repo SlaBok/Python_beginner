770f00fcae62d00a5911df7d3c0edcac85e428c8
SlaBok <113177567+SlaBok@users.noreply.github.com>
2023-11-06 22:14:19 +0100

    Second commit

---
+++ Listy.py
@@ -0,0 +1,13 @@
+countries = ['FR', 'US', 'DE', 'RU']
+
+print(countries[1])
+
+countries.append('PL')
+
+print(countries)
+
+countries.insert(2,'ES')
+
+print(countries)
+
+countries.sort()
---
+++ dictionary.py
@@ -0,0 +1,9 @@
+# podobnie jak w vba
+
+CountryLeaders={'PL':'Duda','US':'Trump'}
+print(CountryLeaders)
+CountryLeaders['DE'] = 'Markel'
+
+print(CountryLeaders.keys())
+print(CountryLeaders.values())
+print(CountryLeaders.items())
